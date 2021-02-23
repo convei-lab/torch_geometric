3e8bd18f24ac87227fd29c3383c4fc7f6cf8c3cf
Hong-in Lee <theorist17@gmail.com>
2021-02-24 08:36:36 +0900

    attack

--- nn/conv/gcn4_conv_sigir.py
+++ nn/conv/gcn4_conv_sigir.py
@@ -374,7 +374,7 @@ class GCN4ConvSIGIR(MessagePassing):
 
         return edge_score, edge_label, new_edge, del_edge
 
-    @staticmethod
+    @ staticmethod
     def loss(model):
 
         loss_list = []
